f7b4dd2d1c039e31da2cce9807eb0071ba48f617
Araldi Garcia <aizendaisho@gmail.com>
2023-03-08 10:29:53 -0400

    contact

--- src/components/Contacto.tsx
+++ src/components/Contacto.tsx
@@ -1,5 +1,42 @@
 import React from "react";
 
 export default function Contacto() {
-  return <div>Contacto</div>;
+  return (
+    <div className="grid m-10">
+
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 place-self-center text-black ">
+      <div className="card-body ">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Nombre</span>
+          </label>
+          <input type="text" placeholder="Nombre" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="text" placeholder="email" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Asunto</span>
+          </label>
+          <input type="text" placeholder="asunto" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Mensaje</span>
+          </label>
+          <textarea  placeholder="Mensaje" className="textarea textarea-bordered resize-none" />
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </div>
+    </div>
+    </div>
+
+
+  );
 }
